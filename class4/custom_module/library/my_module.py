cbe1d878f7089422a7db9addc958ba36d634cc1b
Kirk Byers <ktbyers@twb-tech.com>
2026-01-07 10:55:20 -0800

    Initial working custom module

--- class4/custom_module/library/my_module.py
+++ class4/custom_module/library/my_module.py
@@ -7,6 +7,7 @@ from ansible.module_utils.basic import AnsibleModule
 # Ignore self-signed certificate warnings (note, this is a security issue)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def chkpnt_login(base_url, endpoint, payload, session_id=None, ssl_verify=False):
 
 def chkpnt_api(base_url, endpoint, payload, session_id=None, ssl_verify=False):
     """
@@ -21,7 +22,7 @@ def chkpnt_api(base_url, endpoint, payload, session_id=None, ssl_verify=False):
     response = requests.post(
         url, data=json.dumps(payload), headers=headers, verify=ssl_verify
     )
-    return response.json()
+    return response
 
 
 def run_module():
@@ -29,8 +30,8 @@ def run_module():
     # Define the arguments
     module_args = dict(
         host=dict(type="str", required=True),
-        api_user=dict(type="str", required=False),
-        api_password=dict(type="str", required=False, no_log=True),
+        api_user=dict(type="str", required=True, no_log=False),
+        api_password=dict(type="str", required=True, no_log=True),
     )
 
     # Initialize AnsibleModule object with the arguments
@@ -46,14 +47,17 @@ def run_module():
     # Login to the Mgmt API
     # Use "read-only" to avoid locking the database
     login_payload = {"user": user, "password": password, "read-only": True}
-    login_data = chkpnt_api(base_url=base_url, endpoint="login", payload=login_payload)
-    session_id = login_data.get("sid")
+    response = chkpnt_api(base_url=base_url, endpoint="login", payload=login_payload)
+    # session_id = login_data.get("sid")
 
     results = {
         "changed": False,          # mandatory
         "failed": False,           # optional
-        "msg": "Hello world",
+        "msg": json.dumps(response)
+    #    "msg": response.json()
     }
+    # response = [user]
+    # results["msg"] = response
     module.exit_json(**results)
 
 
